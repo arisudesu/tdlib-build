c98a8f54147ad379677fc752427029a323116207
Maxim Kuleshov <arisudesu@yandex.ru>
2022-04-20 21:14:43 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade
 sudo apt-get install make git zlib1g-dev libssl-dev gperf php-cli cmake g++
 git clone https://github.com/tdlib/td.git
 cd td
-git checkout 92c2a9c4e521df720abeaa9872e1c2b797d5c93f
+git checkout 054a823c1a812ee3e038f702c6d8ba3e6974be9c
 rm -rf build
 mkdir build
 cd build
